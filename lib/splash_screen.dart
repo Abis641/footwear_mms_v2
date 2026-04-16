fff07fac861f91a089ce3fcb3a688415cfe5c1af
abenezer wendemu girma <abiswendemu@gmail.com>
2026-04-16 11:57:21 +0300

    Add SplashScreen with navigation to LoginScreen
    Implement a splash screen that navigates to the login screen after a delay.

---
+++ lib/splash_screen.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/material.dart';
+import 'dart:async';
+import 'auth/login_screen.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    super.initState();
+
+    Timer(const Duration(seconds: 5), () {
+      Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(builder: (_) => const LoginScreen()),
+      );
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              "Footwear MMS",
+              style: TextStyle(
+                fontSize: 32,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            SizedBox(height: 20),
+
+            Text("Powered by:",
+                style: TextStyle(fontSize: 16)),
+
+            SizedBox(height: 10),
+
+            Text("• ABDI NEGESA"),
+            Text("• HENOK WESANU"),
+            Text("• LELISA BAYERA"),
+            Text("• TESFAYE FIKADU"),
+            Text("• YEZINA NURE"),
+          ],
+        ),
+      ),
+    );
+  }
+}
