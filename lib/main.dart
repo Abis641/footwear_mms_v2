6e475c6233a6cc3f4e953f0f5f22b64cc0fc8181
abenezer wendemu girma <abiswendemu@gmail.com>
2026-04-16 11:57:57 +0300

    Replace LoginScreen with SplashScreen

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
-import 'auth/login_screen.dart';
+import 'splash_screen.dart'; // 👈 ADD THIS
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -20,7 +20,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: LoginScreen(),
+      home: SplashScreen(), // 👈 CHANGE HERE
     );
   }
 }
